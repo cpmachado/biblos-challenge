d900d22a0595d65999707157cf73985a9fed31e6
cpmachado <cpmachado@protonmail.com>
2021-02-28 12:30:42 +0000

    [feature/setup] Updating package.json; Adding linting and nodemon

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 const baseMiddlewares = [
   bodyParser.json({
-    limit: "20mb"
-  })
-]
+    limit: '20mb',
+  }),
+];
 
-let articles = require("./data/articles.json");
+app.use(baseMiddlewares);
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App listening on http://127.0.0.1:${port}`));
