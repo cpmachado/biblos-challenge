55abc2d03ff8f3099ab6d7050238222dcd4b6d89
cpmachado <cpmachado@protonmail.com>
2021-02-28 13:46:07 +0000

    [feature/authors] Error middleware, and logging

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const httpLogger = require('morgan');
+
+const { errorNotFoundMiddleware, errorHandlingMiddleware } = require('./util/error');
+const logger = require('./util/logger');
 
 const app = express();
 const port = 3000;
@@ -10,7 +14,17 @@ const baseMiddlewares = [
   }),
 ];
 
+// disable unused headers
+app.disable('x-powered-by');
+app.disable('etag');
+
+// add some request logging
+app.use(httpLogger('combined'));
+
 app.use(baseMiddlewares);
 
-// eslint-disable-next-line no-console
-app.listen(port, () => console.log(`App listening on http://127.0.0.1:${port}`));
+// error handling middleware
+app.use(errorNotFoundMiddleware);
+app.use(errorHandlingMiddleware);
+
+app.listen(port, () => logger.info(`App listening on http://127.0.0.1:${port}`));
